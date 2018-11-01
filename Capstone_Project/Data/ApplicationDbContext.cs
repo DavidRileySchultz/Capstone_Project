28dfac49fc1b51f662cb337eac26ca949f33ea1c
Davidrileyschultz <rileyschultz@gmail.com>
2018-11-01 11:17:16 -0500

    database created with table for travellers

--- Capstone_Project/Data/ApplicationDbContext.cs
+++ Capstone_Project/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Capstone_Project.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ namespace Capstone_Project.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        
+        DbSet<Traveller> Traverllers { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
--- Capstone_Project/Startup.cs
+++ Capstone_Project/Startup.cs
@@ -1,10 +1,13 @@
+using Capstone_Project.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
 
 namespace Capstone_Project
 {
@@ -20,6 +23,10 @@ namespace Capstone_Project
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the React files will be served from this directory
