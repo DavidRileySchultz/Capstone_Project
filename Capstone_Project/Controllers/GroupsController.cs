5455305bb67ea4472602e34e63980db1baf81a26
Davidrileyschultz <rileyschultz@gmail.com>
2018-11-02 10:40:19 -0500

    giving users the ability to see their groups

--- Capstone_Project/Controllers/GroupsController.cs
+++ Capstone_Project/Controllers/GroupsController.cs
@@ -47,6 +47,23 @@ namespace Capstone_Project.Controllers
             }
         }
 
+        [HttpGet("[action]")]
+        public ShowGroupViewModels GetGroups(int id)
+        {
+            var groups = GetGroupsByTravellerId(id);
+            List<ShowGroupViewModel> groupsIn = CreateGroupsInSnapshotForClient(groups);
+            ShowGroupViewModels all = new ShowGroupViewModels();
+            all.groupsIn = groupsIn;
+            return all;
+        }
+
+        private List<Group> GetGroupsByTravellerId(int id)
+        {
+            return _context.GroupMembers.Where(a => a.TravellerId == id)
+                .Join(_context.Groups, a => a.GroupId, b => b.GroupId, (a, b) => new { a, b })
+                .Select(c => c.b).ToList();
+        }
+
         private int FindGroupIdByName(string groupName)
         {
             var thisGroup = _context.Groups.Where(a => a.Name == groupName).ToList();
@@ -56,6 +73,19 @@ namespace Capstone_Project.Controllers
             }
             int thisGroupId = thisGroup[0].GroupId;
             return thisGroupId;
+        }
+
+        private List<ShowGroupViewModel> CreateGroupsInSnapshotForClient(List<Group> groups)
+        {
+            List<ShowGroupViewModel> snapshots = new List<ShowGroupViewModel>();
+            foreach (Group group in groups)
+            {
+                ShowGroupViewModel snapshot = new ShowGroupViewModel();
+                snapshot.Id = group.GroupId;
+                snapshot.Name = group.Name;
+                snapshots.Add(snapshot);
+            }
+            return snapshots;
         }
     }
 }
---
+++ Capstone_Project/ViewModels/ShowGroupViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone_Project.ViewModels
+{
+    public class ShowGroupViewModel
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }   
+
+    public class ShowGroupViewModels
+    {
+        public List<ShowGroupViewModel> groupsIn { get; set; }
+        public List<ShowGroupViewModel> groupsOwn { get; set; }
+    }
+}
+
