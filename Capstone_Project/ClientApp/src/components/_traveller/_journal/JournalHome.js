145e14bb47559613b44badd20b6d3f8037884f40
Davidrileyschultz <rileyschultz@gmail.com>
2018-11-05 09:53:27 -0600

    added in landing page for journal

---
+++ Capstone_Project/ClientApp/src/components/_traveller/_journal/JournalHome.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+function JournalHome() {
+    return (
+        <div>
+            <h2> My Travel Journal </h2>
+            <h4> The world is a book, and those who do not travel only read one page. </h4>
+        </div>
+    );
+}
+
+export default JournalHome;
