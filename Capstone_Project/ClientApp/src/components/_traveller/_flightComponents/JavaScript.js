5742fe2177e091202bddd7a030c5021da5d6c0ed
Davidrileyschultz <rileyschultz@gmail.com>
2018-11-07 09:55:37 -0600

    added new packages for react

---
+++ Capstone_Project/ClientApp/src/components/_traveller/_flightComponents/JavaScript.js
@@ -0,0 +1,18 @@
+﻿import React, { Component } from 'react';
+
+class FlightComponent extends React.Component {
+    render() {
+        return (
+            <div data-skyscanner-widget="SearchWidget"></div>
+        );
+    }
+}
+
+componentDidMount() {
+    window
+}
+
+
+
+<div data-skyscanner-widget="SearchWidget"></div>
+    <script src="https://widgets.skyscanner.net/widget-server/js/loader.js" async></script>
